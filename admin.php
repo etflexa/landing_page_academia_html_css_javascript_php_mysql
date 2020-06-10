4bb7ae8eb7337566c0710f6abaee32510dc2742d
Eduardo <teixeira_wifi@hotmail.com>
2020-06-10 10:44:47 -0300

    primeiro commit da landing page

---
+++ admin.php
@@ -0,0 +1,148 @@
+<?php
+
+	$pdo = new PDO('mysql:host=localhost;dbname=landing_page_db','root','');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+	<style type="text/css">
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: "Lato";
+	}
+
+	body{
+		background: rgb(225,225,225);
+	}
+
+	.header{
+		padding: 10px 0;
+		background: #333;
+		color: white;
+	}
+
+	nav.menu ul{
+		list-style-type: none;
+	}
+
+	nav.menu li{
+		display: inline-block;
+		padding: 0 8px;
+	}
+
+	nav.menu a{
+		color: white;
+	}
+	.logo{
+		float: left;
+	}
+
+	.clear{clear: both;}
+
+	nav.menu{
+		position: relative;
+		top:4px;
+		float: right;
+	}
+
+	.center{
+		max-width: 1100px;
+		margin: 0 auto;
+		padding: 0 2%;
+	}
+
+	
+	.sucesso{
+		width: 100%;
+		margin:10px 0;
+		padding: 8px 15px;
+		color: #3c763d;
+		background: #dff0d8;
+	}
+
+	section.formularios{
+		padding: 30px 0;
+
+	}
+
+	.box-formulario{
+		float: left;
+		width: 33.3%;
+	
+		padding: 10px;
+	}
+	.box-single-wraper{
+		padding: 10px;
+		background: white;
+	}
+
+	.box-single-wraper a{
+		color: black;
+		text-decoration: none;
+	}
+</style>
+	<title>ACADEMIA FIT - ADMINISTRADOR</title>
+</head>
+<body>
+	<div class="header">
+		<div class="center">
+			<div class="logo">
+				<h2>ACADEMIA FIT - PÁGINA DO ADMINISTRADOR</h2>
+			</div>
+
+			<nav class="menu">
+				<ul>
+					<li><a href="">Formulários Abertos</a></li>
+				</ul>
+				
+			</nav>
+
+			<div class="clear"></div>
+			
+		</div>
+	</div>
+
+	<section class="formularios">
+		<div class="center">
+			<?php
+
+				if(isset($_GET['excluir'])) {
+					$id = (int)$_GET['excluir'];
+					$pdo->exec("DELETE FROM `clientes` WHERE id = $id");
+					echo '<div class="sucesso">Formulário deletado com sucesso</div>';
+				}
+
+				$info = $pdo->prepare("SELECT * FROM `clientes`");
+				$info->execute();
+				$info = $info->fetchAll();
+				foreach ($info as $key => $value) {
+
+			?>
+
+			<div class="box-formulario">
+				<div class="box-single-wraper">
+					Nome: <?php echo $value['nome']; ?><br>
+					E-mail: <?php echo $value['email']; ?><br>
+					Telefone: <?php echo $value['telefone']; ?><br>
+					<a href="?excluir=<?php echo $value['id']; ?>">EXCLUIR</a>
+					
+					
+				</div>
+				
+			</div>
+
+			<?php } ?>
+			<div class="clear"></div>
+			
+		</div>
+		
+	</section>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,159 @@
+<?php
+	$pdo = new PDO('mysql:host=localhost;dbname=landing_page_db','root','');
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SITE - ACADEMIA</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+	<meta name='viewport' content='width=device-width,initial-scale=1.0,maximum-scale=1.0'>
+</head>
+<body>
+
+	<div class="header">
+		<div class="center">
+			<img src="images/logo-muscle.png">
+			<div class="title-site">
+			<h2>ACADEMIA FIT - LOREM IPSUM</h2>
+		</div>
+		</div><!--center-->
+		
+		
+	</div><!--header-->
+
+	<div class="container-banner">
+		<div class="center">
+			<div>
+				<div class="container-list">
+					<ul>
+						<li>Os melhores profissionais do mercado</li>
+						<li>Uma estrutura de tirar o fôlego</li>
+						<li>Equipamentos de primeira linha</li>
+						<li>Funcionamento 24h</li>
+					</ul>
+				</div><!--container-list-->
+			</div>
+			<div>
+			<div class="form">
+				<h2>Preencha o formulário e inicie seu preparo no mundo da musculação!</h2>
+				<form method="post">
+
+					<?php
+
+						if(isset($_POST['acao'])){
+							$nome = $_POST['nome'];
+							$email = $_POST['email'];
+							$telefone = $_POST['telefone'];
+							$sql = $pdo->prepare("INSERT INTO `clientes` VALUES (null,?,?,?)");
+							$sql->execute(array($nome,$email,$telefone));
+							echo '<div class="sucesso">Seu formulário foi enviado com sucesso!</div>';
+						}
+
+					?>
+
+					<div class="input-container">
+						<span>Nome*</span>
+						<input type="text" name="nome" required="">
+					</div><!--input-container-->
+
+					<div class="input-container">
+						<span>E-mail*</span>
+						<input type="email" name="email" required="">
+					</div><!--input-container-->
+
+					<div class="input-container ">
+						<span>Telefone</span>
+						<input type="text" name="telefone">
+					</div><!--input-container-->
+
+					<p class="warning-form">*Campos obrigatórios</p>
+
+					<div class="input-submit-container">
+						<input type="submit" name="acao" value="Enviar">
+					</div><!--input-submit-container-->
+
+
+				</form>
+			</div><!--form-->
+		</div>
+			<div class="clear"></div>
+			</div><!--center-->
+	</div><!--container-banner-->
+
+	<div class="container-2">
+		<div class="center">
+			<h2>Lorem Ipsum</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+		</div><!--center-->
+
+		<div class="arrow"></div>
+	</div><!--container-2-->
+
+	<div class="section-1">
+		<div class="center">
+			<h2>Aprenda com quem está no mercado</h2>
+			<div class="container-section">
+				<div class="container-single">
+					<div><img src="images/muscle-img.jpg"></div>
+					<div class="text-container-single">
+					<h2>Lorem ipsum</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore</p>
+					</div><!--text-container-single-->
+				</div><!--container-single-->
+
+				<div class="container-single">
+					<div><img src="images/muscle-img.jpg"></div>
+					<div class="text-container-single">
+					<h2>Lorem ipsum</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore</p>
+					</div><!--text-container-single-->
+				</div><!--container-single-->
+
+				<div class="container-single">
+					<div><img src="images/muscle-img.jpg"></div>
+					<div class="text-container-single">
+					<h2>Lorem ipsum</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore</p>
+					</div><!--text-container-single-->
+				</div><!--container-single-->
+
+				<div class="container-single">
+					<div><img src="images/muscle-img.jpg"></div>
+					<div class="text-container-single">
+					<h2>Lorem ipsum</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore</p>
+					</div><!--text-container-single-->
+				</div><!--container-single-->
+
+				<div class="container-single">
+					<div><img src="images/muscle-img.jpg"></div>
+					<div class="text-container-single">
+					<h2>Lorem ipsum</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore</p>
+					</div><!--text-container-single-->
+				</div><!--container-single-->
+
+				<div class="container-single">
+					<div><img src="images/muscle-img.jpg"></div>
+					<div class="text-container-single">
+					<h2>Lorem ipsum</h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore</p>
+					</div><!--text-container-single-->
+				</div><!--container-single-->
+
+				<div class="clear"></div>
+			</div><!--container-section-->
+		</div><!--center-->
+	</div><!--section-1-->
+
+	<div class="footer">
+		<p>Todos os direitos reservados</p>
+		<p>Av. Tereza Maciel Tavares, nº267, Jardim Marco Zero, cep:68902513, Macapá/AP</p>
+	</div><!--footer-->
+
+</body>
+
+</html>
